b58d11b9d153c8e5a9ee5c74d34114a187925c43
gitboy2022 <106193845+gitboy2022@users.noreply.github.com>
2024-07-30 00:03:54 +0000

    do some cw

--- USACO Grind/Class 10/main.py
+++ USACO Grind/Class 10/main.py
@@ -1,4 +1,20 @@
 N = int(input())
 
+def check_moo(s):
+  for i in range(len(s)):
+    if s[i:i + 3] == "MOO":
+      return len(s) - 3
+  for i in range(len(s)):
+    if s[i:i + 3] == "MOM":
+      return len(s) - 2
+    if s[i:i + 3] == "OOO":
+      return len(s) - 2
+  for i in range(len(s)):
+    if s[i:i + 3] == "OOM":
+      return len(s) - 1
+    
+
 for i in range(N):
-    pass
+  s = input()
+  print(check_moo(s))
+  
