cb31c6a14ca241dc2fac8695151bc2ff0bf96255
gitboy2022 <106193845+gitboy2022@users.noreply.github.com>
2024-08-06 00:03:52 +0000

    add hw

---
+++ USACO Grind/Class 11/hw#1.py
@@ -0,0 +1,89 @@
+#HW#18
+
+#Remember, you get 80% for submitting your
+#classwork before the session ends.
+#See below to see the requirements for getting +10% and +20%
+
+#NOTE: This is due at 12AM EST of the next time / day we meet!!!
+#NOTE2: Please submit your USACO / Codeforces answer to the website 
+#If it does not pass ALL test cases, your answer will be considered
+#INCORRECT!!!
+
+
+#Requirements to get +10% :
+#  Correctly answer AT LEAST 2 questions for #1.  Leetcode Questions
+
+#Requirements to get +20% :
+#  Correctly answer ALL questions for #1.  Leetcode Questions (There are 4 Questions)
+#  Correctly answer #2.  USACO - COW
+
+
+#1.  Leetcode Questions
+
+#Solve the following Leetcode Questions
+#as was given in the instructions from HW#9 / HW#10
+
+
+
+#a.  Find Common Characters
+
+# https://leetcode.com/problems/find-common-characters/description/
+
+###COPY / PASTE Find Common Characters
+
+
+###END Find Common Characters
+
+
+
+
+#b.  N-th Tribonacci Number
+
+# https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+
+###COPY / PASTE N-th Tribonacci Number
+
+
+###END N-th Tribonacci Number
+
+
+
+
+#c.  Three Consective Odds
+
+# https://leetcode.com/problems/three-consecutive-odds/description/
+
+###COPY / PASTE Three Consecutive Odds
+
+
+###END Three Consecutive Odds
+
+
+
+#d.  Find Common Elements Between Two Arrays
+
+# https://leetcode.com/problems/find-common-elements-between-two-arrays/description/
+
+
+###COPY / PASTE Find Common Elements Between Two Arrays
+
+
+###END Find Common Elements Between Two Arrays
+
+
+
+
+#2.  USACO - COW
+
+#Please answer the following USACO question below.
+
+#MAKE SURE TO SUBMIT YOUR FILE / SOLUTION to USACO
+#AND CHECK IT WORKS COMPLETELY!
+
+#IF IT DOESN'T WORK COMPLETELY (AS IN PASSES ALL TEST CASES)
+#YOU DO NOT RECEIVE CREDIT FOR THIS PROBLEM!!!
+
+#GOOD LUCK!
+
+# https://usaco.org/index.php?page=viewproblem2&cpid=527
