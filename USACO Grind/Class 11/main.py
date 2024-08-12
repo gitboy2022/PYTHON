673d619163f19f4b036dfe10abf9ed81e3e93c3a
gitboy2022 <106193845+gitboy2022@users.noreply.github.com>
2024-08-12 23:58:58 +0000

    mooloo

--- USACO Grind/Class 11/main.py
+++ USACO Grind/Class 11/main.py
@@ -1,5 +1,13 @@
 N, K = map(int, input().split(" "))
 days = [int(x) for x in input().split(" ")]
 
+total = 0
 for i in range(N):
-    pass
+  if i == 0:
+    total += K + 1
+  else:
+    extend_cost = days[i] - days[i - 1]
+    sub_cost = K + 1
+    cheapest = min(extend_cost, sub_cost)
+    total += cheapest
+print(total)
