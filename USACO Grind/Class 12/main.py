144fbe08cf389287bc42cc3c4f4a420ed8ac472a
Brian Ou <106193845+gitboy2022@users.noreply.github.com>
2024-09-09 20:00:44 -0400

    Update main.py

--- USACO Grind/Class 12/main.py
+++ USACO Grind/Class 12/main.py
@@ -1,7 +1,19 @@
 import sys
 
-sys.stdin = open("cow.in", "r")
-sys.stdout = open("cow.out", "w")
+sys.stdin=open('cow.in', 'r')
+sys.stdout=open('cow.out', 'w')
 
-N = input()
-word = input()
+N = int(input())
+# print(N)
+phrase = input()
+
+C, O, W = 0, 0, 0
+for i in range(N):
+    ltr = phrase[i]
+    if ltr == 'C':
+        C+=1
+    if ltr == 'O':
+        O+=C
+    if ltr == 'W':
+        W+=O
+print(W)
